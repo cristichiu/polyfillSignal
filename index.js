e27cce36a289c51f0e77781abd5de3ffa163eaec
cristichiu <grajdiancristian@gmail.com>
2025-04-09 15:40:56 +0300

    adding more examples

--- index.js
+++ index.js
@@ -50,15 +50,34 @@ const Signal = require("./signal.js")
 // b.set(2)
 // console.log(display.get())
 
-const a = new Signal.State(0)
-const a1 = new Signal.State(0)
-a1.set(3)
-const b = new Signal.Computed(() => { console.log(1); return !(a.get()%2) })
-const c = new Signal.Computed(() => { console.log(2); return b.get() ? "even":"odd" })
-const s = new Signal.Computed(() => {
-    console.log(3)
-    return `${c.get()} is ${a.get()}`
-})
-console.log(s.get())
-a.set(2)
-console.log(s.get())
+// const a = new Signal.State(0)
+// const a1 = new Signal.State(0)
+// a1.set(3)
+// const b = new Signal.Computed(() => { console.log(1); return !(a.get()%2) })
+// const c = new Signal.Computed(() => { console.log(2); return b.get() ? "even":"odd" })
+// const s = new Signal.Computed(() => {
+//     console.log(3)
+//     return `${c.get()} is ${a.get()}`
+// })
+// console.log(s.get())
+// a.set(1)
+// console.log(s.get())
+
+// const a = new Signal.State(0)
+// const b = new Signal.State(0)
+// const a1 = new Signal.Computed(() => {console.log(1); return a.get()})
+// const b1 = new Signal.Computed(() => {console.log(2); return b.get()})
+// let i = 0
+// const d = new Signal.Computed(() => {
+//     console.log(3)
+//     if(i) {
+//         return a1.get()
+//     } else {
+//         return b1.get()
+//     }
+// })
+// console.log("result", d.get())
+// a.set(1)
+// i = 1
+// b.set(2)
+// console.log("result", d.get())
